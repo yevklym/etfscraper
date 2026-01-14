2567829fd917ab3f60a64ef544d01f947d96bee3
Yevhenii Klymenko <klymenko@campus.tu-berlin.de>
2026-01-14 17:55:28 +0100

    test: add unit tests for iShares client configuration options

---
+++ internal/providers/ishares/options_test.go
@@ -0,0 +1,68 @@
+package ishares
+
+import (
+	"testing"
+	"time"
+)
+
+func TestClientOptions(t *testing.T) {
+	t.Run("default configuration", func(t *testing.T) {
+		c, err := New("us")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if c.httpConfig.Timeout != 15*time.Second {
+			t.Errorf("expected default timeout 15s, got %v", c.httpConfig.Timeout)
+		}
+
+		if c.httpConfig.Debug != false {
+			t.Error("expected debug to be false by default")
+		}
+	})
+
+	t.Run("WithTimeout option", func(t *testing.T) {
+		c, err := New("us", WithTimeout(30*time.Second))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if c.httpConfig.Timeout != 30*time.Second {
+			t.Errorf("expected timeout 30s, got %v", c.httpConfig.Timeout)
+		}
+	})
+
+	t.Run("WithDebug option", func(t *testing.T) {
+		c, err := New("us", WithDebug(true))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !c.httpConfig.Debug {
+			t.Error("expected debug to be true")
+		}
+	})
+
+	t.Run("multiple options", func(t *testing.T) {
+		mockClient := &mockHTTPClient{StatusCode: 200}
+
+		c, err := New("us",
+			WithHTTPClient(mockClient),
+			WithTimeout(45*time.Second),
+			WithDebug(true),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if c.httpConfig.Client != mockClient {
+			t.Error("expected custom HTTP client")
+		}
+		if c.httpConfig.Timeout != 45*time.Second {
+			t.Errorf("expected timeout 45s, got %v", c.httpConfig.Timeout)
+		}
+		if !c.httpConfig.Debug {
+			t.Error("expected debug to be true")
+		}
+	})
+}
